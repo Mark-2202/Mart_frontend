44bb3328c8bb01650cd95c3ef61488fb744ba83b
Mark-2202 <mark@example.com>
2026-06-02 16:12:08 +0700

    deploy frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mini-mart-api.onrender.com/api',
+  apiUrl: 'https://mart-backend-ten.vercel.app/',
   appName: 'Mini Mart POS',
   version: '1.0.0'
 };
